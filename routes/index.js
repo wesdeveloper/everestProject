53ea21ab4b7e54c1911a4db53eb9a51ee6599020
Wesley <wesleylucas13@gmail.com>
2016-11-16 02:18:41 -0200

    routes portolio 1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
   var indexController = app.controllers.IndexController;
   app.get('/',                      indexController.index);
   app.get('/cases',                 indexController.cases);
-  app.get('/portfolio',             indexController.portfolio);
+  //app.get('/portfolio',             indexController.portfolio);
   app.get('/comunicacao-interna',   indexController.ComunicacaoInterna);
   app.get('/comunicacao-externa',   indexController.ComunicacaoExterna);
   app.get('/envelopamento',         indexController.Envelopamento);
